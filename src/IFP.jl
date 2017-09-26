4b8fa3e2ba60ac1356455ebd9e486ef6ccdc68ae
Chase <cc7768@gmail.com>
2017-09-26 00:02:01 -0400

    Not converging yet

--- src/IFP.jl
+++ src/IFP.jl
@@ -46,8 +46,10 @@ function IFP_Sol()
     # Guess policy of
     # atp1 = 0.9*at + 0.15*wt
     #      1    x    x^2  x^3  x^2y xy  xy^2  y    y^2  y^3
-    c_a = [0.0, 0.9, 0.0, 0.0, 0.0, 0.0, 0.0, 0.15, 0.0, 0.0]
-    c_V = zeros(10)
+    c_a = [0.0, 0.75, 0.0, 0.0, 0.35, 0.0]
+    c_V = zeros(6)
+    # c_a = [0.0, 0.75, 0.0, 0.0, 0.0, 0.0, 0.0, 0.35, 0.0, 0.0]
+    # c_V = zeros(10)
 
     return IFP_Sol(c_a, c_V)
 end
@@ -63,12 +65,13 @@ function eval_V!(temp::Vector{Float64}, m::IFP, pol::IFP_Sol,
     β, ρ, σ = m.β, m.ρ, m.σ
     nodes, weights = m.nodes, m.weights
     c_V = pol.c_V
+    d = BasisMatrices.Degree{2}()
 
     # Compute expected value
     EV = 0.0
     for (ϵ, weight) in zip(nodes, weights)
         wtp1 = w^ρ * exp(σ*ϵ)
-        EV += weight * dot(c_V, complete_polynomial!(temp, [atp1, wtp1], 3))
+        EV += weight * dot(c_V, complete_polynomial!(temp, [atp1, wtp1], d))
     end
 
     return u(m, expendables_t(m, a, w) - atp1) + β*EV
@@ -79,7 +82,8 @@ end
 Takes a given policy and simulates it N times for T periods... It then stores the
 Tth observation of each chain
 """
-function simulate(m::IFP, policy::IFP_Sol, N::Int, T::Int)
+function simulate(m::IFP, policy::IFP_Sol, N::Int, T::Int,
+                  a0::Float64, w0::Float64)
     # Pull out relevant information
     ρ, σ = m.ρ, m.σ
     c_a = policy.c_a
@@ -87,13 +91,13 @@ function simulate(m::IFP, policy::IFP_Sol, N::Int, T::Int)
     #
     # Simulate given a policy and store last period of each simulation
     #
-    temp = Array{Float64}(n_complete(2, 3))  # Hard coded two dimensions and cubic polys
+    temp = Array{Float64}(n_complete(2, 2))  # Hard coded two dimensions and cubic polys
+    d = BasisMatrices.Degree{2}()
     sim_out = Array{Float64}(2, N)
-    d = BasisMatrices.Degree{3}()
 
-    srand(42)
+    srand(42)  # Use same seed each iteration
     for n in 1:N
-        a, w = 0.0, 1.0
+        a, w = a0, w0
         for t in 1:T
             a = dot(c_a, complete_polynomial!(temp, [a, w], d))
             w = w^ρ * exp(σ*randn())
@@ -114,11 +118,12 @@ Builds a grid using the cluster grids described in Maliar Maliar 2015
 Uses the most basic format which uses a k means algorithm to classify
 points into k clusters.
 """
-function build_grid(m::IFP, policy::IFP_Sol, k::Int, N::Int, T::Int)
+function build_grid(m::IFP, policy::IFP_Sol, k::Int, N::Int, T::Int,
+                    a0::Float64, w0::Float64)
     #
     # Simulate
     #
-    sim_out = simulate(m, policy, N, T)
+    sim_out = simulate(m, policy, N, T, a0, w0)
 
     #
     # Use simulation output to build the grid
@@ -136,18 +141,27 @@ function iterate_given_policy!(
 
     # Initialize counters
     dist, iter = 10.0, 0
-    temp = Array{Float64}(n_complete(2, 3))
-    V_upd = copy(V)
-    d = BasisMatrices.Degree{3}()
+    temp = Array{Float64}(n_complete(2, 2))
+    copy!(policy.c_V, zeros(n_complete(2, 2)))  # Always start guess at all zeros
+
+    # Initialize basis matrix
+    Φ = complete_polynomial(G', 2)  # Get big basis matrix
+    Φ2 = view(Φ, :, 2:size(Φ, 2))
+
+    # Initialize types to speed things up
+    d = BasisMatrices.Degree{2}()
+    reg = RLSSVD(intercept=true)
 
     while (dist > tol) & (iter < maxiter)
         for i in 1:k
             a_t, w_t = G[:, i]
             atp1 = dot(policy.c_a, complete_polynomial!(temp, [a_t, w_t], d))
-            V_upd[i] = eval_V!(temp, m, policy, a_t, w_t, atp1)
+            V[i] = eval_V!(temp, m, policy, a_t, w_t, atp1)
         end
-        dist = maximum(abs, V - V_upd)
-        copy!(V, V_upd)
+
+        c_V_upd = regress(reg, Φ2, V)
+        dist = maximum(abs, policy.c_V - c_V_upd)
+        copy!(policy.c_V, c_V_upd)
     end
 
     return V
@@ -160,16 +174,16 @@ function solve(
 
     # Initialize counters
     dist, iter = 10.0, 0
-    d = BasisMatrices.Degree{3}()
+    d = BasisMatrices.Degree{2}()
     reg = RLSSVD(intercept=true)
 
     # Allocate some array space
-    G = build_grid(m, policy, k, N, T)
-    Φ = complete_polynomial(G', 3)  # Get big basis matrix
+    G = build_grid(m, policy, k, N, T, 0.0, 1.0)
+    Φ = complete_polynomial(G', 2)  # Get big basis matrix
     Φ2 = view(Φ, :, 2:size(Φ, 2))
     Astar = Array{Float64}(k)
     V = Array{Float64}(k)
-    temp = Array{Float64}(n_complete(2, 3))
+    temp = Array{Float64}(n_complete(2, 2))
 
     # First to get a sensible value function, iterate to convergence for the first policy
     iterate_given_policy!(V, m, policy, G, k)
@@ -180,16 +194,18 @@ function solve(
     while (dist > tol) & (iter < maxiter)
 
         # Rebuild the grid
-        println("Rebuilding grid")
-        ts = time()
-        G = build_grid(m, policy, k, N, T)
-        te = time() - ts
-        println("\tRebuilding grid took $(round(te, 2)) seconds")
-        println("\tNew grid extrema are $(extrema(G, 2))")
+        if (dist < 1e-2) & (iter % 15 == 0)
+            println("Rebuilding grid")
+            ts = time()
+            G = build_grid(m, policy, k, N, T, maximum(G[1, :]), maximum(G[2, :]))
+            te = time() - ts
+            println("\tRebuilding grid took $(round(te, 2)) seconds")
+            println("\tNew grid extrema are $(extrema(G, 2))")
 
-        # Build full basis matrix
-        Φ = complete_polynomial(G', 3)
-        Φ2 = view(Φ, :, 2:size(Φ, 2))
+            # Build full basis matrix
+            Φ = complete_polynomial(G', 2)
+            Φ2 = view(Φ, :, 2:size(Φ, 2))
+        end
 
         # Iterate over states
         for i_s in 1:k
@@ -198,25 +214,27 @@ function solve(
 
             # Maximize by minimizing the negative value
             lb, ub = 0.0, expendables_t(m, a_t, w_t) - 1e-8
-            res = golden_method(
+            res = optimize(
                 atp1 -> -eval_V!(temp, m, policy, a_t, w_t, atp1), lb, ub
                )
 
             # Update policy
-            Astar[i_s] = res[1]
+            # if ~res.converged
+            #     println("Failed to converge for $i_s")
+            # end
+            Astar[i_s] = res.minimizer
         end
 
         # Update coefficients and compute distance
         c_a_upd = regress(reg, Φ2, Astar)
-        copy!(policy.c_a, (1.0-δ)*policy.c_a .+ δ*c_a_upd)
         dist = maximum(abs, policy.c_a - c_a_upd)
+        copy!(policy.c_a, (1.0-δ)*policy.c_a .+ δ*c_a_upd)
         iter = iter + 1
-        println("After $iter the distance is $dist")
-        println("New policy is $(policy.c_a)")
+        println("After $iter the distance is $dist with coeffs $(policy.c_a)")
+        # println("New policy is $(policy.c_a)")
 
-        # Iterate on VF a few times to update value and make it sensible
+        # Iterate on VF using the new policy
         iterate_given_policy!(V, m, policy, G, k)
-        copy!(policy.c_V, Φ\V)
     end
 
     return policy
@@ -226,9 +244,9 @@ end
 
 #=
 # Initialize stuff
-m = IFP()
+m = IFP(r=0.035)
 policy = IFP_Sol()
 
-solve(m, policy; tol=1e-6, maxiter=500)
+solve(m, policy; δ=0.15, tol=1e-6, maxiter=1500, k=75, N=1500, T=750)
 
 =#
